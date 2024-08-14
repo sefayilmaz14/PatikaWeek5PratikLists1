c7f1150ea704e3de8204497773827654dec336c2
sefayilmaz <sefaylmaz14@gmail.com>
2024-08-14 17:03:38 +0300

    Add project files.

---
+++ PatikaWeek5PratikLists1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//Liste Tanımlası yapıldı
+List<string> guest = new List<string>();
+
+//Listeye elemean girişleri yapıldı
+guest.Add("Bülent Ersoy");
+guest.Add("Ajda Pekkan");
+guest.Add("Ebru Gündeş");
+guest.Add("Hadise");
+guest.Add("Hande Yener");
+guest.Add("Tarkan");
+guest.Add("Funda Arar");
+guest.Add("Demet Akalın");
+
+//For Dögüsü ile listedeki elemanlar ekrana yazdırıldı
+for(int i  = 0; i < guest.Count; i++)
+{
+    Console.WriteLine($"{i + 1}-){guest[i]}");
+}
+
